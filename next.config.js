b50ca44f65f9be937f8fb80e5318e0f176d441ef
Jeff Chew <jeff.chew@gmail.com>
2020-04-22 19:01:25 -0400

    feat(dotenv): adds dotenv to the project

--- next.config.js
+++ next.config.js
@@ -1,24 +1,25 @@
-const path = require('path');
-const withCSS = require('@zeit/next-css');
-const withSass = require('@zeit/next-sass');
-const withProgressBar = require('next-progressbar');
+require("dotenv").config();
+const path = require("path");
+const withCSS = require("@zeit/next-css");
+const withSass = require("@zeit/next-sass");
+const withProgressBar = require("next-progressbar");
 
 const styleLoaders = [
   {
-    loader: 'postcss-loader',
+    loader: "postcss-loader",
     options: {
       plugins: () => {
-        const autoPrefixer = require('autoprefixer')({
-          overrideBrowserslist: ['last 1 version', 'ie >= 11'],
+        const autoPrefixer = require("autoprefixer")({
+          overrideBrowserslist: ["last 1 version", "ie >= 11"],
         });
         return [autoPrefixer];
       },
     },
   },
   {
-    loader: 'sass-loader',
+    loader: "sass-loader",
     options: {
-      includePaths: [path.resolve(__dirname, 'node_modules')],
+      includePaths: [path.resolve(__dirname, "node_modules")],
       data: `
       $feature-flags: (
         ui-shell: true,
@@ -32,15 +33,15 @@ const styleLoaders = [
 module.exports = withProgressBar(
   withSass(
     withCSS({
-      assetPrefix: '.',
+      assetPrefix: ".",
       env: {
-        CORS_PROXY: 'https://dds-proxy.mybluemix.net/',
-        ROOT_PATH: process.env.ROOT_PATH || '/',
+        CORS_PROXY: process.env.CORS_PROXY || "",
+        ROOT_PATH: process.env.ROOT_PATH || "/",
       },
       sassLoaderOptions: {
-        includePaths: [path.resolve(__dirname, 'node_modules')],
+        includePaths: [path.resolve(__dirname, "node_modules")],
       },
-      webpack: config => {
+      webpack: (config) => {
         config.module.rules.push({
           test: /\.scss$/,
           sideEffects: true,
