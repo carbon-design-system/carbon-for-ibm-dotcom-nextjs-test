386d63e11b7527f3feed2eb14c23bebe6afc0aa4
Diego Marcuz <Diego.Marcuz@ibm.com>
2020-07-01 15:52:47 -0300

    feat(ServicesPage): added one section added ContentBlockCards and CardLink

--- pages/services.js
+++ pages/services.js
@@ -8,6 +8,8 @@ import {
   LeadSpace,
   TableOfContents,
   ContentBlockSegmented,
+  ContentBlockCards,
+  CardLink,
 } from "@carbon/ibmdotcom-react";
 
 import React from "react";
@@ -92,6 +94,64 @@ const Services = () => (
             — Donec quis pretium odio, in dignissim sapien.`,
           },
         ]}
+      />
+      <a
+        name="content-block-cards-and-card-Link"
+        data-title="Content Block Cards and Card Link"
+      />
+      <ContentBlockCards
+        heading="Content Block Cards and Card Link"
+        cards={[
+          {
+            image: {
+              defaultSrc:
+                "https://dummyimage.com/1056x792/ee5396/161616%26text=4:3",
+              alt: "Image alt text",
+            },
+            eyebrow: "Lorem",
+            heading: "Lorem ipsum dolor sit amet, consectetur adip possib",
+            cta: {
+              href: "https://www.example.com",
+            },
+          },
+          {
+            image: {
+              defaultSrc:
+                "https://dummyimage.com/792x1056/ee5396/161616%26text=3:4",
+              alt: "Image alt text",
+            },
+            eyebrow: "Lorem",
+            heading: "Lorem ipsum dolor sit amet, consectetur adip possib",
+            cta: {
+              href: "https://www.example.com",
+            },
+          },
+          {
+            image: {
+              defaultSrc:
+                "https://dummyimage.com/1056x1056/ee5396/161616%26text=1:1",
+              alt: "Image alt text",
+            },
+            eyebrow: "Lorem",
+            heading: "Lorem ipsum dolor sit amet, consectetur adip possib",
+            cta: {
+              href: "https://www.example.com",
+            },
+          },
+        ]}
+      />
+
+      <CardLink
+        card={{
+          copy: "Services",
+          cta: {
+            type: "local",
+            href: "https;//www.ibm.com/services",
+            icon: {
+              src: ArrowRight20,
+            },
+          },
+        }}
       />
     </TableOfContents>
   </>
